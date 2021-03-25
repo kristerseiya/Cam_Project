ebb4a5d0907c1ba5c2e424a86d0cf5a7e96e677c
Krister Seiya Ulvog <al.krse.u.6471@gmail.com>
2021-03-25 10:58:53 -0400

    Update station_example_main.c

--- client/main/station_example_main.c
+++ client/main/station_example_main.c
@@ -24,8 +24,8 @@
    If you'd rather not, just change the below entries to strings with
    the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
 */
-#define EXAMPLE_ESP_WIFI_SSID      "Apt Big 10"
-#define EXAMPLE_ESP_WIFI_PASS      "B01l3rUp!"
+#define EXAMPLE_ESP_WIFI_SSID      "xxxxx"
+#define EXAMPLE_ESP_WIFI_PASS      "xxxxx"
 #define EXAMPLE_ESP_MAXIMUM_RETRY  CONFIG_ESP_MAXIMUM_RETRY
 
 /* FreeRTOS event group to signal when we are connected*/
